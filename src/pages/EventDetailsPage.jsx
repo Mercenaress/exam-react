04dc1989479305470cf76472760bd2f68885c39c
Mercenaress <linden9707@gmail.com>
2023-04-12 11:04:29 +0200

    Styling edits

--- src/pages/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage.jsx
@@ -4,15 +4,15 @@ import styles from './EventDetailsPage.module.css'
 
 function EventDetailsPage() {
     return ( 
-        <>
-        <h3>Event</h3>
-        <p>You are about to score some tickets to</p>
-        <h2>Lasse-Stefanz</h2>
-        <p>21 mars kl 19:00-21:00</p>
-        <p>@Kjell Härnqvistsalen</p>
-        <EventPriceSummary priceSummary={"1050 sek"} ticketAmount={1} />
-        <GreenButton buttonText={"Lägg i varukorgen"} />
-        </>
+        <section className={styles.EventDetailsPage} >
+            <h3>Event</h3>
+            <p>You are about to score some tickets to</p>
+            <h2>Lasse-Stefanz</h2>
+            <p>21 mars kl 19:00-21:00</p>
+            <p>@Kjell Härnqvistsalen</p>
+            <EventPriceSummary priceSummary={"1050 sek"} ticketAmount={1} />
+            <GreenButton buttonText={"Lägg i varukorgen"} />
+        </section>
      );
 }
 
