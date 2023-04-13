609a5437e97d7484c61a63ebcbccf0049252b28b
larssonrd <94178885+larssonrd@users.noreply.github.com>
2023-04-13 12:32:23 +0200

    Added CartContext and necessary functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import './App.css'
+import { CartProvider } from './context/CartContext';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import './App.css';
+import OrderPage from './pages/OrderPage';
 
 function App() {
-
   return (
-    <div className="App">
-     
-    </div>
-  )
+    <CartProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<OrderPage />} />
+        </Routes>
+      </Router>
+    </CartProvider>
+  );
 }
 
-export default App
+export default App;
