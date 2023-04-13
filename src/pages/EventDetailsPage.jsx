49fa8b091c3b20e2d71a1ecb53d8c9f28caaf022
Mercenaress <linden9707@gmail.com>
2023-04-13 11:15:27 +0200

    Styling eventdetail page

--- src/pages/EventDetailsPage.jsx
+++ src/pages/EventDetailsPage.jsx
@@ -6,11 +6,11 @@ function EventDetailsPage() {
     return ( 
         <div className={styles.EventDetailsPage}>
         <h3>Event</h3>
-        <p>You are about to score some tickets to</p>
+        <p className={styles.HeadText} >You are about to score <br/> some tickets to</p>
         <section>
             <h2>Lasse-Stefanz</h2>
-            <p>21 mars kl 19:00-21:00</p>
-            <p>@Kjell Härnqvistsalen</p>
+            <p className={styles.TealText} >21 mars kl 19:00-21:00</p>
+            <p className={styles.LocationText} >@Kjell Härnqvistsalen</p>
         </section>
         <EventPriceSummary priceSummary={"1050 sek"} ticketAmount={1} />
         <GreenButton buttonText={"Lägg i varukorgen"} />
